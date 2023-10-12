811e7438103aa8ae053626c296bb0049ecfebe97
Sunnythakur01 <135720207+Sunnythakur01@users.noreply.github.com>
2023-10-12 23:20:51 +0530

    initial change
    initial change

---
+++ large.js
@@ -0,0 +1,29 @@
+// const str ="SUNNY";
+// const reversed = reverseString(str)
+// document.write(reversed)
+
+// function reverseString(str){
+//     return str.split("").reverse().join("");
+// }
+
+
+
+
+// const arr =[20,25,70,24,20,90]
+// function findmax (max,curr){
+
+//     if( curr > max){
+//         max=curr;
+//     }
+//     return max;
+// }
+
+// const output =arr.reduce(findmax)
+// document.write(output);
+
+
+
+//palindrome,,,,>
+
+
+
---
+++ scope closer.js
@@ -0,0 +1,70 @@
+// massage ="good"
+// function hello(){
+//   {
+//         let massage =" good afternoon"
+//         console.log("hello : "+ massage)
+//     }
+//     console.log(massage)
+// }
+// hello()
+
+// let p = 9
+// function ax(){
+//     var a = 8;
+//     console.log(a)
+
+// }
+// ax ()
+// console.log(p)
+
+
+
+// function outer(){
+//     let sunny = "sunil"
+//     function inner (){
+//         console.log("inner , sunny")
+//     }
+//     inner()
+// }
+// outer()
+
+
+// document.getElementById("orange").onclick = function(){
+//     document.body.style.backgroundColor = "orange"
+// }
+
+// function clickHandler(color){
+//     return function (){
+//         document.body.style.backgroundColor = `${color}`
+//     }
+// }
+// document.getElementById("orange").onclick = clickHandler 
+// ("orange")
+// document.getElementById("red").onclick = clickHandler 
+// ("red")
+// document.getElementById("black").onclick = clickHandler 
+// ("black")
+// document.getElementById("blue").onclick = clickHandler 
+// ("blue")
+// document.getElementById("green").onclick = clickHandler 
+// ("green")
+// document.getElementById("yellow").onclick = clickHandler 
+// ("yellow")
+
+
+// const str ="SUNNY";
+// const reversed = reverseString(str)
+// document.write(reversed)
+
+// function reverseString(str){
+//     return str.split("").reverse().join("");
+// }
+
+// document.write('SUNNY')
+
+
+for(var a=1 ; a <=100 ; a++){
+    if(a % 2  == 0 ){
+    document.write(a + "<br>");
+    }
+}
